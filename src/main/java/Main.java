3fa54e5bd5ef7364c3a1225e2692cc502583373b
dtelega <dima.hannasay@gmail.com>
2018-05-11 19:56:32 +0300

    add some train prog, fix is needed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,43 +16,70 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.io.*;
 import java.util.Scanner;
+import java.io.BufferedReader;
 
 import static algo.Util.*;
 
 public class Main {
-    private static float theta0 = 0;
-    private static float theta1 = 0;
+    private static int theta0 = 0;
+    private static int theta1 = 0;
 
-    private static float estimatePrice(int mileage, float tmpTheta0, float tmpTheta1) {
+    private static int estimatePrice(int mileage, int tmpTheta0, int tmpTheta1) {
         return tmpTheta0 + (tmpTheta1 * mileage);
     }
 
+    private static int findLengthFile(String datasetPath) {
+        int m = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(datasetPath));
+            while (reader.readLine() != null) m++;
+            reader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return m;
+    }
     private static void startTraining(String datasetPath) {
-        float tmp0 = theta0;
-        float tmp1 = theta1;
-        int m = 24; // TODO m!!!!!!!!!
-        
+        int tmp0;
+        int tmp1;
+        int i = 0;
 
+        int m = findLengthFile(datasetPath);
+        int x [] = new int[m];
+        int y [] = new int[m];
         try {
             Scanner sc = new Scanner(new File(datasetPath));
-            int km;
+            
             String [] splitted;
-            int price;
-
             splitted = sc.nextLine().split(",");
             while(sc.hasNext()){
                 splitted = sc.nextLine().split(",");
-                km = Integer.parseInt(splitted[0]);
-                price = Integer.parseInt(splitted[1]);
-                System.out.println(km + ","+price+"      "+tmp0+" "+tmp1);
-                tmp0 = (1/m) * (estimatePrice(km, tmp0, tmp1) - price);
-                tmp1 = (1/m) * (estimatePrice(km, tmp0, tmp1) - price) * km;
-                theta0 = tmp0;
-                theta1 = tmp1;
+                x[i] = Integer.parseInt(splitted[0]);
+                y[i] = Integer.parseInt(splitted[1]);
+                i++;
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+/*
+        // check x y
+        for (int j = 0; j < i; j++)
+            System.out.println(x[j]+","+y[j]);
+        // end
+*/
+        for (int q = 200; q>0; q--) {
+            tmp0 = 0;
+            tmp1 = 0;
+            for (int j = 0; j < m; j++) {
+                tmp0 += (estimatePrice(x[j], theta0, theta1) - y[j]);
+                tmp1 += (estimatePrice(x[j], theta0, theta1) - y[j]) * x[j];
+                //System.out.println("tmp = "+tmp0+"   "+tmp1);
+            }
+            theta0 -= (tmp0 * 3 / 10) / m;
+            theta1 -= (tmp1 * 3 / 10) / m;
+            System.out.println(theta0+"    "+theta1);
+        }
+        System.out.println(theta0+"    "+theta1);
     }
 
 
@@ -69,24 +96,25 @@ public class Main {
                 fr.close();
                 String[] subStr;
                 subStr = b.split(" ");
-                theta0 = Float.parseFloat(subStr[0]);
-                theta1 = Float.parseFloat(subStr[1]);
+                theta0 = Integer.parseInt(subStr[0]);
+                theta1 = Integer.parseInt(subStr[1]);
             } catch (IOException ex) {
                 ex.printStackTrace();
+                System.out.println("Error");
+                System.exit(0);
             }
 
         }
         else {
             try {
                 FileWriter writer = new FileWriter(thetasData);
-                writer.write("0.0 0.0");
+                writer.write("0 0");
                 writer.close();
 
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
-
         Options options = parseArguments(args);
         String datasetPath;
         String programType;
@@ -116,6 +144,15 @@ public class Main {
         }
         else {
             startTraining(datasetPath);
+            try {
+                FileWriter writer = new FileWriter(thetasData);
+                writer.write(theta0 + " " + theta1);
+                writer.close();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
+
     }
 }
